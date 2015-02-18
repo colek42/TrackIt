1797a61b3c86203761782cd380826e18ad654d2f
Joshua Pinos <pinosjxp@users.noreply.github.com>
2015-02-18 13:02:13 -0500

    Update ImageCropperForHog.py
    Cleaned up code. Changed how the program listens for keyboard actions. Also added more functionality and specified them in the beginning of the code(i.e. Can jump to next frame without saving a p/n image, change size of region of interest for selecting positive and negative images).

--- ImageCropperForHog.py
+++ ImageCropperForHog.py
@@ -1,7 +1,15 @@
 """
 This program takes an input directory that contains video files of the sample data that needs to be processed for HOG Training.
 All positive and negative images will be created in the same directory as the videos.
-Positive images are noted with _p/_n for positive and negative respectively, and _f, where f is the frame number the image was derived from.
+Sample images are annotated with _p/_n for positive and negative respectively, and _f, where f is the frame number the image was derived from.
+
+Keyboard Commands:
+========================================================================
+V = Next frame
+N = Save as negative image
+SpaceBar = Save as positive image
+< = Decrease roi
+> = Increase roi
  """
 
 import cv2
@@ -30,6 +38,8 @@ def VideoCropper(filename):
         frameno=frameno+1
     
     while(cap.isOpened()):
+        width=64
+        height=128
         ret, frame = cap.read()
         tmpframe=np.array(frame)
         cv2.setMouseCallback('frame',draw_roi)
@@ -40,28 +50,53 @@ def VideoCropper(filename):
             if (visible):
                 if(init):
                     tmpframe=np.array(frame)
-                    cv2.rectangle(tmpframe,(gx,gy),(gx+64,gy+128),(255,0,0),1)
+                    cv2.rectangle(tmpframe,(gx,gy),(gx+width,gy+height),(255,0,0),1)
                     init=False
                     
-            if cv2.waitKey(10) & 0xFF == ord(' ') and visible:
+            k = cv2.waitKey(10) & 0xFF
+# Save roi defined by mouse as a 64x128 JPEG image; Image name is annotated as a positive image.
+            if k== ord(' ') and visible:
                 print("Positive")
-                roi=frame[gy:gy+128,gx:gx+64]
+                roi=frame[gy:gy+height,gx:gx+width]
+                resized_roi = cv2.resize(roi, (64,128))
                 saveName=filename[0:-4]+"_"+str(frameno)+"_p.jpeg"
-                cv2.imwrite(saveName,roi)
+                cv2.imwrite(saveName,resized_roi)
                 break
-            if cv2.waitKey(20) & 0xFF == ord('n') and visible:
+# Save roi defined by mouse as a 64x128 JPEG image; Image name is annotated as a negative image.
+            if k== ord('n') and visible:
                 print("Negative")
-                roi=frame[gy:gy+128,gx:gx+64]
+                roi=frame[gy:gy+height,gx:gx+width]
+                resized_roi = cv2.resize(roi, (64,128))
                 saveName=filename[0:-4]+"_"+str(frameno)+"_n.jpeg"
-                cv2.imwrite(saveName,roi)
+                cv2.imwrite(saveName,resized_roi)
                 break
+# Decreases size of roi.
+            if k== ord(','):
+                if width>=16:
+                    width=width-4
+                    height=height-8
+                    tmpframe=np.array(frame)
+                    cv2.rectangle(tmpframe,(gx,gy),(gx+width,gy+height),(255,0,0),1)
+                    print("Width:"+str(width)+ " Height:"+str(height))
+# Increases size of roi.
+            if k== ord('.'):
+                if width<=128:
+                    width=width+4
+                    height=height+8
+                    tmpframe=np.array(frame)
+                    cv2.rectangle(tmpframe,(gx,gy),(gx+width,gy+height),(255,0,0),1)
+                    print("Width:"+str(width)+ " Height:"+str(height))
+# Skips to next frame.
+            if k== ord('v'):
+                break
+                
         visible=False
         frameno=frameno+1
         
     cap.release()
     cv2.destroyAllWindows()
     
-# Mouse callback event for getting area to be cropped
+# Mouse callback event for getting area to be cropped.
 def draw_roi(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         global gx,gy,visible,init
@@ -70,7 +105,7 @@ def draw_roi(event,x,y,flags,param):
         visible=True
         init=True        
         
-# Main start of program        
+# Main start of program.        
 inputDir=raw_input("Enter the name of the directory where the video files are located:\n")
 os.chdir(inputDir)
 print(os.getcwd())
